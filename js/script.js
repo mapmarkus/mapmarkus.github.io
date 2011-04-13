1c8b2525c387f45a39837eda8677b075957108ee
Marcos Arias <marias@coffeecup.com>
2011-04-13 13:05:37 +0200

    New content and markup

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("footer em").before( function() {
+    var email = $(this).attr('title').replace(" /at/ ", "@").replace(" /dot/ ", ".");
+    return $("<a href='mailto:" + email + "'/>").text($(this).text());
+  }).remove();
+});
