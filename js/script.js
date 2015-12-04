3a6bd18d4fca72e62125deb62a9c953581d2c447
Markus A. Stone <yizzreel@gmail.com>
2015-12-04 16:20:35 +0100

    Clean up template

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $("#intro em").before( function() {
-    var email = $(this).attr('title').replace(" /at/ ", "@").replace(" /dot/ ", ".");
-    return $("<a href='mailto:" + email + "'/>").text($(this).text());
-  }).remove();
-});
